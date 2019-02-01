acb7937bc1aee05d4832c93d988c97a966b09af5
Sébastien Labine <sebastienlabine@aol.com>
2019-02-01 13:04:42 -0500

    Updated for newest version of moodle

--- clone-moodle.py
+++ clone-moodle.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# encoding=utf8 
+# encoding=utf8
 import getpass
-import os,sys,zipfile,time
+import os
+import sys
+import zipfile
+import time
 
 if sys.version[0] == '2':
     reload(sys)
@@ -28,16 +31,21 @@ except NameError:
 
 # Constants
 LOGIN_URL = 'https://moodle.polymtl.ca/login/index.php'
-SIGLES_COURS = ['AER','ELE','GLQ','IFT','MTH','SPL','CAP','ENE','GML','IND','MEC','MTR','TPE','EST','INF','MET','PHS','SLI','SST','SSH','STI','BIO','CHE','DDI','GBM','ING','SMC','CIV','GCH','LOG']
-RESTRICTED_CHARS = ['*','\\','/','?',':','|','<','>','\"']
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+SIGLES_COURS = ['AER', 'ELE', 'GLQ', 'IFT', 'MTH', 'SPL', 'CAP', 'ENE', 'GML', 'IND', 'MEC', 'MTR', 'TPE', 'EST',
+                'INF', 'MET', 'PHS', 'SLI', 'SST', 'SSH', 'STI', 'BIO', 'CHE', 'DDI', 'GBM', 'ING', 'SMC', 'CIV', 'GCH', 'LOG']
+RESTRICTED_CHARS = ['*', '\\', '/', '?', ':', '|', '<', '>', '\"']
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+           'Accept-Encoding': 'gzip, deflate, br',
+           }
 SESSION = requests.session()
+
 
 def verifyString(name):
     for char in RESTRICTED_CHARS:
         if char in name:
-            name = name.replace(char,"")
+            name = name.replace(char, "")
     return name
+
 
 def createFolder(foldername):
     # Fix for the error : NotADirectoryError : [WinError 267]
@@ -47,69 +55,74 @@ def createFolder(foldername):
         os.makedirs(foldername)
     else:
         print("Le dossier " + foldername + " existe deja")
-    os.chdir(foldername)
 
-def request(url,cookie):
-    return SESSION.post(url,headers=HEADERS, cookies=cookie)
+
+def request(url, cookie):
+    return SESSION.post(url, headers=HEADERS, cookies=cookie)
+
 
 def fetchAllClasses(html):
     # Allows us to find every classes
-    classesFetch = html.find_all('div','course_title')
-         
+    classesFetch = html.find_all('h4')
+
     # Cleaning up and keeping only the important classes
+    classesFetch = (list(set(classesFetch)))
     linksToPages = []
-    for i in range(0,len(classesFetch)):
-        # Compare it with the array of classes prefix
-        if classesFetch[i].text[:3] in SIGLES_COURS:
-            linksToPages.append(classesFetch[i].h2.a.attrs.get('href'))
-    
-    if len(linksToPages) == 0:
-        print("Erreur de connexion : mauvais nom d'utilisateur ou mauvais mot de passe")
-        sys.exit()
-    else:
-        print("Succes lors de la connexion au serveur Moodle...\n")
-        print("Vous avez " + str(len(linksToPages)) + " cours.\n")
+    for i in range(0, len(classesFetch)):
+        if classesFetch[i].a.text[:3] in SIGLES_COURS:
+            linksToPages.append(classesFetch[i].a.attrs.get('href'))
+    print("Vous avez " + str(len(linksToPages)) + " cours.\n")
     return linksToPages
 
+
 def findRessourceTab(html):
-    tabs = html.find("ul",attrs={"class":"unlist"}).find_all("li")
+    tabs = html.find("ul", attrs={"class": "unlist"}).find_all("li")
 
     for tab in tabs:
         if('resources' in tab.a.get('href')):
             linkTab = tab.a.get('href')
-        else :
+        else:
             linkTab = "none"
     return linkTab
 
 
-def savefile(title,documentlink,cookie):
-    reqDocument = request(documentlink,cookie)
+def savefile(title, documentlink, cookie):
+    reqDocument = request(documentlink, cookie)
     filetype = reqDocument.headers['content-type'].split('/')[-1]
 
     # IF the file is code or a page
-    if('charset=utf-8' in filetype):
+    if('charset=utf-8' in filetype.lower()):
         try:
-            html = BeautifulSoup(reqDocument.text,"html.parser").find('div',attrs={'class':'resourceworkaround'})
+            html = BeautifulSoup(reqDocument.text, "html.parser").find(
+                'div', attrs={'class': 'resourceworkaround'})
             html.a.get('href')
         except AttributeError:
             filetypeArray = reqDocument.url.split('.')
             filetype = filetypeArray[len(filetypeArray)-1]
-            
+
             # Don't download if it's a page
             if('id' in filetype):
                 return None
-            
             if('?forcedownload=1' in filetype):
-                filetype = filetype.replace('?forcedownload=1',"")
-                
-        else :
+                filetype = filetype.replace('?forcedownload=1', "")
+        else:
             link = html.a.get('href')
-            reqDocument = request(link,cookie)
+            reqDocument = request(link, cookie)
             filetype = reqDocument.headers['content-type'].split('/')[-1]
 
     if('msword' in filetype):
         filetype = 'doc'
+    if('vnd.ms-powerpoint' in filetype):
+        filetype = 'ppt'
+    if("vnd.openxmlformats-officedocument.wordprocessingml.document" in filetype):
+        filetype = 'docx'
 
+    if("vnd.openxmlformats-officedocument.presentationml.slideshow" in filetype):
+        filetype = 'ppsx'
+
+    if("gzip" in filetype):
+        filetype = 'tar.gz'
+    
     # Fix for the error : FileNotFoundError: [Errno 2] No such file or directory
     title = verifyString(title)
 
@@ -118,97 +131,88 @@ def savefile(title,documentlink,cookie):
         title = title[:230]
 
     with open(title + "." + filetype, "wb") as file:
-            file.write(reqDocument.content)
+        file.write(reqDocument.content)
 
-def findAllDocuments(html,cookie):
-    documents = html.find_all("td", attrs={"class":"c1"})
 
+def findAllDocuments(html, cookie):
+    documents = html.find_all("td", attrs={"class": "cell c1"})
     for document in documents:
         title = document.a.text[1:]
-        linkDocument = document.a.get('href')
-        if('resource' in linkDocument):
-            print(title)
-            savefile(title,linkDocument,cookie)
-        # verify if there's a folder    
-        else:
-            if ('folder' in linkDocument):
-                req = request(linkDocument,cookie)
-                dossierHTML = BeautifulSoup(req.text,"html.parser")
-                title = dossierHTML.find('div',attrs={'role':'main'}).h2.text
-                createFolder(title)
-                id = dossierHTML.find('input',attrs={'name':'id'}).get('value')
-                savefile(title,"https://moodle.polymtl.ca/mod/folder/download_folder.php?id="+id,cookie)
-                
-                # Download the file under a zip file
-                zip = zipfile.ZipFile(title + ".zip")
-                zip.extractall()
-                zip.close()
-                
-                # Delete the zip file
-                os.remove(title + ".zip")
-                os.chdir("..")
-            
-def main():
+        documentURL = document.a.get('href')
+        if('resource' in documentURL):
+            print("Téléchargement du fichier " + title + "...")
+            savefile(title, documentURL, cookie)
+        if("folder" in documentURL):
+            req = request(documentURL, cookie)
+            folderHTML = BeautifulSoup(req.text, "html.parser")
+            folderName = folderHTML.find("div", {"role": "main"}).h2.text
+            print(folderName)
+            createFolder(folderName)
+            os.chdir(folderName)
+            folderId = documentURL.split("=")[1]
+            savefile(folderName, "https://moodle.polymtl.ca/mod/folder/download_folder.php?id="+folderId, cookie)
+            # Download the file under a zip file
+            zip = zipfile.ZipFile(folderName + ".zip")
+            zip.extractall()
+            zip.close()
+            # Delete the zip file
+            os.remove(folderName + ".zip")
+            os.chdir("..")
 
+
+def accessResourceURL(classURL, cookie):
+    resourceURL = classURL.replace("view","resources")
+    req = request(resourceURL, cookie)
+    return BeautifulSoup(req.text, "html.parser")
+
+def main():
     startTime = time.time()
     # User information
     username = input("Veuillez entrer votre nom d'utilisateur\n")
     password = getpass.getpass('Veuillez entrer votre mot de passe \n')
-
 
     # Data to send for the login
     data = {
         'username': username,
         'password': password,
         'rememberusername': 1,
-        'loginbtn' : 'Connexion'
+        'loginbtn': 'Connexion'
     }
-
-    # Creating the folder
-    createFolder("Polytechnique Montreal")
-
-
     # Login
-    loginRequest = SESSION.post(LOGIN_URL,data=data, headers=HEADERS)
+    loginRequest = SESSION.post(LOGIN_URL, data=data, headers=HEADERS)
     loginCookie = loginRequest.cookies
 
     # Redirecting to home page
-    url = "https://moodle.polymtl.ca/my/index.php?mynumber=-2"  # URL that shows all classes
-    home = request(url,loginCookie)
-    homeHTML = BeautifulSoup(home.text,"html.parser")
-    classes = fetchAllClasses(homeHTML)
+    # URL that shows all classes
+    url = "https://moodle.polymtl.ca/my/index.php?mynumber=-2"
+    home = request(url, loginCookie)
+    homeHTML = BeautifulSoup(home.text, "html.parser")
+    if(homeHTML.title.text == "Tableau de bord"):
+        print("Login Succesful")
+        # Creating the folder
+        createFolder("Polytechnique Montreal")
+        os.chdir("Polytechnique Montreal")
 
-    # Navigating through each class
-    for thisClass in classes:
-        req = request(thisClass,loginCookie)
-        thisClassHTML = BeautifulSoup(req.text,"html.parser")
+        classes = fetchAllClasses(homeHTML)
 
-        # Getting the class title
-        title = thisClassHTML.title.text[8:]
+        # Navigating through each class
+        for currentClass in classes:
+                req = request(currentClass, loginCookie)
+                currentClassHTML = BeautifulSoup(req.text, "html.parser")
+                title = currentClassHTML.title.text[8:]
+                createFolder(title)
+                os.chdir(title)
+                resourceHTML = accessResourceURL(currentClass, loginCookie)
+                findAllDocuments(resourceHTML, loginCookie)
+                os.chdir("..")
+    else:
+        print("Erreur de connexion : mauvais nom d'utilisateur ou mauvais mot de passe")
+        sys.exit()
 
-        # Creating the class folder
-        createFolder(title)
-    
-        # Trying to access the ressource tab
-        ressourceLink = findRessourceTab(thisClassHTML)
-
-        # Accessing the ressource page
-        if(ressourceLink != "none"):
-            req = request(ressourceLink,loginCookie)
-        else:
-            # Skipping the class if there's no ressource tab
-            os.chdir("..")
-            continue
-        ressourceHTML = BeautifulSoup(req.text,"html.parser")
-        
-        findAllDocuments(ressourceHTML,loginCookie)
-        os.chdir("..")
-    os.chdir("..")
-    os.chdir("..")
     endTime = time.time()
 
-    print("Fin de la tache. Duree totale :" + "{0:.2f}".format(round(endTime-startTime),2) + " secondes")
-
+    print("Fin de la tache. Duree totale :" +
+          "{0:.2f}".format(round(endTime-startTime), 2) + " secondes")
 
 if __name__ == "__main__":
     main()
